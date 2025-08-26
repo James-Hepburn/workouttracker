e225faf3314aa34a743c0afedafb17bd8ec9c7b0
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-08-25 20:48:11 -0600

    Security

--- src/main/java/com/example/workouttracker/security/CustomUserDetailsService.java
+++ src/main/java/com/example/workouttracker/security/CustomUserDetailsService.java
@@ -1,4 +1,27 @@
 package com.example.workouttracker.security;
 
-public class CustomUserDetailsService {
+import com.example.workouttracker.model.User;
+import com.example.workouttracker.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername (String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByUsername (username).orElseThrow (() -> new UsernameNotFoundException ("User not found"));
+        return new org.springframework.security.core.userdetails.User (
+                user.getUsername (),
+                user.getPassword (),
+                user.getRoles ().stream ()
+                        .map (role -> new SimpleGrantedAuthority (role.getName ()))
+                        .collect (Collectors.toList ()));
+    }
 }
--- src/main/java/com/example/workouttracker/security/JwtUtils.java
+++ src/main/java/com/example/workouttracker/security/JwtUtils.java
@@ -1,4 +1,38 @@
 package com.example.workouttracker.security;
 
+import io.jsonwebtoken.*;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
 public class JwtUtils {
+    private String jwtSecret = "K9f#3nQ8pL2vW7tX!zRm4uE6sH1bG0yP";
+    private int jwtExpirationMs = 86400000;
+
+    public String generateJwtToken( String username) {
+        return Jwts.builder ()
+                .setSubject (username)
+                .setIssuedAt (new Date ())
+                .setExpiration (new Date ((new Date ()).getTime () + this.jwtExpirationMs))
+                .signWith (SignatureAlgorithm.HS512, this.jwtSecret)
+                .compact ();
+    }
+
+    public String getUserNameFromJwtToken (String token) {
+        return Jwts.parser ()
+                .setSigningKey (this.jwtSecret)
+                .parseClaimsJws( token)
+                .getBody ()
+                .getSubject ();
+    }
+
+    public boolean validateJwtToken (String authToken) {
+        try {
+            Jwts.parser ().setSigningKey (this.jwtSecret).parseClaimsJws (authToken);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
 }
